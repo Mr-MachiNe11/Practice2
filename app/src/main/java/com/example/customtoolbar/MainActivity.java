e20962e15a8d1a3c748f9b52a4d0223c57e66ca0
machina <zrakib11@gmail.com>
2023-05-02 21:37:49 +0600

    Custom Button

--- app/src/main/java/com/example/customtoolbar/MainActivity.java
+++ app/src/main/java/com/example/customtoolbar/MainActivity.java
@@ -4,29 +4,42 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Toolbar toolbar;
+    Button btnNext;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         toolbar = findViewById(R.id.toolbar);
+        btnNext = findViewById(R.id.btnNext);
 
         //step1
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("My Toolbar");
         toolbar.setSubtitle("Sub Title");
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+            }
+        });
 
 
     }
